164b6b18fffab48405483f9be60a3cb874f69e29
zivcoh12 <zivcoh12@gmail.com>
2024-12-11 12:48:48 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,10 +1,10 @@
-def create_codon_dict(file_path):
-  codon_to_aa = {}
-  with open(file_path, 'r') as file:
-      rows = file.readlines()
-      for row in rows:
-        parts = row.strip().split('\t')
-        codon = parts[0]
-        amino_acid = parts[2]
-        codon_to_aa[codon] = amino_acid
-  return codon_to_aa
+  def create_codon_dict(file_path):
+    codon_to_aa = {}
+    with open(file_path, 'r') as file:
+        rows = file.readlines()
+        for row in rows:
+          parts = row.strip().split('\t')
+          codon = parts[0]
+          amino_acid = parts[2]
+          codon_to_aa[codon] = amino_acid
+    return codon_to_aa
